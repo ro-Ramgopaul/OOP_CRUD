f9a27668437ead6cd43de4c5fe5206914b347bb4
Rohan <rohanzed4@gmail.com>
2018-11-22 13:37:09 -0500

    Initial commit

---
+++ php-oop-crud-level-1/layout_footer.php
@@ -0,0 +1,18 @@
+</div>
+<!-- /container -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous">
+    </script>
+
+<!-- bootbox library -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+
+</body>
+</html>
